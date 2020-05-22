820392976525d7c0617811bff1dd52e73dba294c
yuanzp2 <yuan2366>
2020-05-22 22:51:40 +0800

    分页

--- demo/demo-common/src/main/java/com/springcloud/demo/common/DemoCommonApplication.java
+++ demo/demo-common/src/main/java/com/springcloud/demo/common/DemoCommonApplication.java
@@ -1,16 +1,15 @@
 package com.springcloud.demo.common;
 
-import com.springcloud.demo.common.entity.User;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.springcloud.demo.common.mapper.UserMapper;
 import com.springcloud.demo.common.result.Result;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @SpringBootApplication
@@ -25,8 +24,9 @@ public class DemoCommonApplication {
 
     @GetMapping("")
     public Result test(){
-        List<User> list = userMapper.selectList(null);
-        return Result.success(list);
+        Page page = new Page(1,10);
+        IPage iPage = userMapper.selectPage(page,null);
+        return Result.success(iPage);
     }
 
 }
