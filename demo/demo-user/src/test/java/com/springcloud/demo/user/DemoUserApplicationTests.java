84c34c9f371a36b86ae6ea0c5f75ef12fd499f12
yuanzp2 <yuan2366>
2020-05-29 20:00:46 +0800

    添加用户模块demo-user

---
+++ demo/demo-user/src/test/java/com/springcloud/demo/user/DemoUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.demo.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
