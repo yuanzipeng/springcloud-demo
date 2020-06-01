efd1a2e3ed2ac052681f38eba3b8657ad8bfd775
yuanzp2 <yuan2366>
2020-06-01 20:53:49 +0800

    用户模块加实体和测试类

---
+++ demo/demo-user/src/main/java/com/springcloud/demo/user/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.springcloud.demo.user.mapper;
+
+import com.springcloud.demo.user.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author yuanzp
+ * @since 2020-05-22
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
