cbf609c226e23cb3095f1521789a3fe56d19a5d6
yuanzp2 <yuan2366>
2020-06-16 14:18:44 +0800

    增加spring-cloud-gateway网关模块

--- demo/spring-cloud-eureka/src/main/java/com/springcloud/demo/eureka/EurekaApplication.java
+++ demo/spring-cloud-eureka/src/main/java/com/springcloud/demo/eureka/EurekaApplication.java
@@ -1,15 +1,21 @@
 package com.springcloud.demo.eureka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@Slf4j
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> log.info("启动shutdownHook对程序进行关闭...")));
     }
+
+
 
 }
