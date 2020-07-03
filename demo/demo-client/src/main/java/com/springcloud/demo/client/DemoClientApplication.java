4527f0e9b6716857c49608b8aea275406314db6f
yuanzp2 <yuan2366>
2020-07-03 22:31:01 +0800

    加***启动成功***访问地址

--- demo/demo-client/src/main/java/com/springcloud/demo/client/DemoClientApplication.java
+++ demo/demo-client/src/main/java/com/springcloud/demo/client/DemoClientApplication.java
@@ -1,17 +1,23 @@
 package com.springcloud.demo.client;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "com.springcloud.demo")
 public class DemoClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoClientApplication.class, args);
+        SpringApplication app = new SpringApplication(DemoClientApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("***启动成功***访问地址：\thttp://localhost:{}{}/swagger-ui.html",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> log.info("启动shutdownHook对程序进行关闭...")));
     }
 
 }
--- demo/demo-user/src/main/java/com/springcloud/demo/user/DemoUserApplication.java
+++ demo/demo-user/src/main/java/com/springcloud/demo/user/DemoUserApplication.java
@@ -1,12 +1,14 @@
 package com.springcloud.demo.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.env.Environment;
 
-
+@Slf4j
 @EnableFeignClients
 @EnableDiscoveryClient
 @MapperScan(basePackages = "com.springcloud.demo.user.mapper")
@@ -14,6 +16,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class DemoUserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoUserApplication.class, args);
+        SpringApplication app = new SpringApplication(DemoUserApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("***启动成功***访问地址：\thttp://localhost:{}{}/swagger-ui.html",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> log.info("启动shutdownHook对程序进行关闭...")));
     }
 }
