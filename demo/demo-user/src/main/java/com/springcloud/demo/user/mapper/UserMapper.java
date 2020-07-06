f770ce9c264e36719da37372cb99a13052949603
yuanzp2 <yuan2366>
2020-07-06 10:37:44 +0800

    修改配置

--- demo/demo-user/src/main/java/com/springcloud/demo/user/mapper/UserMapper.java
+++ demo/demo-user/src/main/java/com/springcloud/demo/user/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.springcloud.demo.user.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.springcloud.demo.api.user.entity.User;
 
+import java.util.List;
+
 /**
  * <p>
  * 用户表 Mapper 接口
@@ -13,4 +15,5 @@ import com.springcloud.demo.api.user.entity.User;
  */
 public interface UserMapper extends BaseMapper<User> {
 
+    List<User> queryList();
 }
